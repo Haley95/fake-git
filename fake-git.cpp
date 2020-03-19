42be6ac506d49db17318c698dc5e71adfb659928
haley <haleyhenson76@yahoo.com>
2020-03-18 23:50:12 -0500

    first commit

---
+++ fake-git.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+using namespace std;
+int main(int argc, char * argv[])
+{
+    int add;
+std::ifstream file("C:./path/to/fake-git.cpp");
+
+  if (argv[3][0] == 3)
+    {
+        cout<< "That is the correct amount of parameters." << endl;
+    }
+    else if (argv[3][0] != 3)
+    {
+        cout<< "That is not the right amount of parameters." << endl;
+    }
+    else if (argv[3][0] == add && "./path/to/fake-git.cpp")
+    {
+        cout<< "Added ./path/to/fake-git.cpp" << endl;
+    }
+    return 0;
+
+}
